6f1257076155ae10b3204b894971707eb98d6cd0
Gabri1391 <chiarellogabriele@gmail.com>
2022-09-15 20:35:23 +0200

    dynamic card added

--- resources/views/includes/main.blade.php
+++ resources/views/includes/main.blade.php
@@ -1,9 +1,29 @@
+@php 
+$comics = config('comics')
+@endphp
+
 <!DOCTYPE html>
 <html lang="en">
  @include('includes.head')
 <body>
     <main>
-
+        <section class="banner">
+            <div class="card-box container">
+                @foreach ($comics as $comic)
+                <div class="comics-card pt-5">
+                    <figure>
+                        <img src="{{ asset ($comic['thumb']) }}" alt="">
+                    </figure>
+                    <figcaption>
+                    <p>{{ strtoupper($comic['title']) }}</p>
+                    </figcaption>
+                </div>
+                @endforeach
+            </div>
+            <div class="button">
+                <a href="#">LOAD MORE</a>
+            </div>
+        </section>
     </main>
 </body>
 </html>
