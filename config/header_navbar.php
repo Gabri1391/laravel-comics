327d3211fab711e9479d0569f725f270db4c6f25
Gabri1391 <chiarellogabriele@gmail.com>
2022-09-15 18:03:54 +0200

    header added

---
+++ config/header_navbar.php
@@ -0,0 +1,15 @@
+<?php
+return[
+    ['text'=>'character','route_name'=>'CHARACTERS'],
+    ['text'=>'comics','route_name'=>'COMICS'],
+    ['text'=>'movies','route_name'=>'MOVIES'],
+    ['text'=>'tv','route_name'=>'TV'],
+    ['text'=>'games','route_name'=>'GAMES'],
+    ['text'=>'colectibles','route_name'=>'COLECTIBLES'],
+    ['text'=>'videos','route_name'=>'VIDEOS'],
+    ['text'=>'fans','route_name'=>'FANS'],
+    ['text'=>'news','route_name'=>'NEWS'],
+    ['text'=>'shop','route_name'=>'SHOP'],
+
+    ]
+?>
