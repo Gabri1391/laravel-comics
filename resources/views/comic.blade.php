c3e8a2ae20d61e73dd479df8197fd98cae44a2a7
Gabri1391 <chiarellogabriele@gmail.com>
2022-09-16 21:22:58 +0200

    css fixing

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -25,11 +25,11 @@
 </section>
 
 <section class="credit">
-    <div class=" table container d-flex">
-        <div class="row d-flex justify-content-between mt-3">
+    <div class=" ms-5 container d-flex">
+        <div class="row d-flex justify-content-between align-items-top mt-3">
             <div class="col mt-5">
                 <h4>TALENT</h4>
-                <div class="row mt-3 mb-3 d-flex">
+                <div class="row mt-3 mb-3 d-flex border-bottom-grey align-items-top">
                     <div class="col">Art By:</div>
                     <div class="col">
                         @foreach ($comic['artists'] as $artist)
@@ -37,7 +37,7 @@
                         @endforeach
                     </div>
                 </div>
-                <div class="row d-flex">
+                <div class="row d-flex border-bottom-grey align-items-top">
                     <div class="col">Written By:</div>
                     <div class="col">
                         @foreach ($comic['writers'] as $writer)
@@ -48,25 +48,20 @@
                 </div>
             </div>
         </div>
-        <div class="row d-flex justify-content-between mt-3">
+        <div class="row d-flex justify-content-between  align-items-top  mt-3">
             <div class="col mt-5">
                 <h4>SPECS</h4>
-                <div class="row mt-3 mb-3 d-flex">
-                    <div class="col">Art By:</div>
-                    <div class="col">
-                        @foreach ($comic['artists'] as $artist)
-                        <span>{{$artist}}</span>@if(!$loop->last), @endif
-                        @endforeach
-                    </div>
+                <div class="row mt-3 mb-3 d-flex justify-content-between align-items-top  border-bottom-grey">
+                    <div class="col">Series:</div>
+                    <div class="col lightblue">{{ $comic['series'] }}</div>
                 </div>
-                <div class="row d-flex">
-                    <div class="col">Written By:</div>
-                    <div class="col">
-                        @foreach ($comic['writers'] as $writer)
-                        <span>{{$writer}}</span>@if(!$loop->last), @endif
-                        @endforeach
-                    </div>
-    
+                <div class="row d-flex justify-content-between  align-items-top border-bottom-grey">
+                    <div class="col">U.S. Price:</div>
+                    <div class="col lightblue">{{ $comic['price'] }}</div>
+                </div>
+                <div class="row d-flex justify-content-between align-items-top  border-bottom-grey">
+                    <div class="col">On Sale Date:</div>
+                    <div class="col lightblue">{{ $comic['sale_date'] }}</div>
                 </div>
             </div>
         </div>
